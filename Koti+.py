c9e942a2904fe0638a881e181819a8fd3a18ab89
Ekaterina43 <ecaterina938@gmail.com>
2023-08-23 17:48:10 +0300

    Initial commit

---
+++ Koti+.py
@@ -0,0 +1,9 @@
+from Kotiki import Kotiki
+
+kot1 = Kotiki('Барон', 'мальчик', 'сибирская', 2, 'без дома')
+
+print("Имя kot1 -", kot1.name, "пол kot1 -", kot1.gender, "порода kot1 -", kot1.breed, "возраст kot1 -", kot1.age, "статус kot1 -", kot1.status)
+
+kot2 = Kotiki('Сэм', 'мальчик', 'сибирская', 2, 'не приютили')
+
+print("Имя kot2 -", kot2.name, "пол kot2 -", kot2.gender, "порода kot2 -", kot2.breed, "возраст kot2 -", kot2.age, "статус kot2 -", kot2.status)
